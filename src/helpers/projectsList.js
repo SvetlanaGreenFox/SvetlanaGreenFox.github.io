6c559bef2216814ed27ca7e57e5285dad4863427
Svetlana <svetlanagreenfox@gmail.com>
2023-09-19 22:31:27 +0300

    delete img

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,9 +1,5 @@
 import project01 from "./assets/project01.png";
-import project01Big from "./assets/project01-big.jpg";
-
 import project02 from "./assets/project02.png";
-import project02Big from "./assets/project02-big.jpg";
-
 import project03 from "./assets/project03.png";
 
 const projects = [
@@ -11,21 +7,18 @@ const projects = [
         title: "Landing Sup",
         skills: "html, css",
         img: project01,
-        imgBig: project01Big,
         gitHubLink: "https://github.com/SvetlanaGreenFox/landing-page_SUP"
     },
     {
         title: "Landing Loft House",
         skills: "html, css",
         img: project02,
-        imgBig: project02Big,
         gitHubLink: "https://github.com/SvetlanaGreenFox/landing-page_loft"
     },
     {
         title: "ToDo List",
         skills: "React, TypeScript, React Router, Material UI",
         img: project03,
-        imgBig: project02Big,
         gitHubLink: "https://github.com/SvetlanaGreenFox/todo"
     }
 ];
